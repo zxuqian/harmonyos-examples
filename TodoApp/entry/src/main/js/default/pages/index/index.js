538a4472cb24c231b0d406ac6aac5ca1d7b814ba
Xuqian Zhang <zxuqian@gmail.com>
2020-09-19 21:01:44 +0800

    Add data binding

--- TodoApp/entry/src/main/js/default/pages/index/index.js
+++ TodoApp/entry/src/main/js/default/pages/index/index.js
@@ -1,8 +1,6 @@
 export default {
     data: {
-        title: ""
+        filters: ["全部", "已完成", "未完成"],
+        todos: [{id: 1, content: "待办1"}, {id: 2, content: "待办2"}]
     },
-    onInit() {
-        this.title = this.$t('strings.world');
-    }
 }
